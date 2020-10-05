7bc4fe3b99ef268c9a7bf944096c571a15633206
angpao <angpao.kittimasak@gmail.com>
2020-10-05 16:17:26 +0700

    cors

--- app.js
+++ app.js
@@ -6,21 +6,21 @@ const http = require('http');
 const app = express()
 app.use(express.json())
 app.use(cors())
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
 app.use('/products', productRoutes)
 
-// const httpServer = http.createServer(app);
+const httpServer = http.createServer(app);
 
-const port = 4000
-app.listen(port, () => {
-    console.log('Listening on port ' + port)
-})
+// const port = 4000
+// app.listen(port, () => {
+//     console.log('Listening on port ' + port)
+// })
 
-// httpServer.listen(4000, () => {
-// 	console.log('HTTP Server running on port 4000');
-// });
+httpServer.listen(4000, () => {
+	console.log('HTTP Server running on port 4000');
+});
